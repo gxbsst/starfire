d242e6b0fd59d295154b6f24cca634c2ee1d4ec1
gxbsst <gxbsst@gmail.com>
2013-04-02 14:41:58 +0800

    init project

---
+++ js/productsdetail.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(
+function()
+{
+  $("#productdetailmenu li").mouseover(
+  function()
+  {
+    $("#productdetailmenu li").removeClass("current");
+    $(this).addClass("current");
+    $("#product_detail01").hide();
+    $("#product_detail02").hide();
+    $("#product_detail03").hide();
+    $("#product_detail04").hide();
+    $("#product_detail05").hide();
+    $("#product_detail06").hide();
+    $("#product_detail0" + $(this).attr("tabindex")).show();
+  }
+  );
+}
+);
+function show_flash(url)
+{
+ $("#flashcontents").show();
+ document.getElementById("falsh_iframe").src = url;
+}
+function close_flash()
+{
+  $("#flashcontents").hide();
+  document.getElementById("falsh_iframe").src = "#";
+}
+var imageheight = 505;
+var imagewidth = 880;
+document.write("<style>* html{background-image: url(image.jpg);}#flashcontents,.flashcontents{position:fixed;_position:absolute;top:" + (document.documentElement.clientHeight - imageheight) / 2 + "px;_top:expression(" + (document.documentElement.clientHeight - imageheight) / 2 + "+((e=document.documentElement.scrollTop)?e:document.body.scrollTop)+'px');_top:expression(" + (document.documentElement.clientHeight - imageheight) / 2 + "+((e=document.documentElement.scrollTop)?e:document.body.scrollTop)+'px');left:" + (document.documentElement.clientWidth - imagewidth) / 2 + "px; _left:expression(" + (document.documentElement.clientWidth - imagewidth) / 2 + "+((e=document.documentElement.scrollLeft)?e:document.body.scrollLeft)+'px');_left:expression(" + (document.documentElement.clientWidth - imagewidth) / 2 + "+((e=document.documentElement.scrollLeft)?e:document.body.scrollLeft)+'px');}</style>");
---
+++ js/select.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(
+function()
+{
+  $(".selectcontent").mouseover(
+  function()
+  {
+    $(this).addClass("selectcontent_current");
+  }
+  );
+  $(".selectcontent").mouseout(
+  function()
+  {
+    $(this).removeClass("selectcontent_current");
+  }
+  );
+  $(".selectcontent .none li").click(
+  function()
+  {
+    $(this).parent().parent().parent().parent().find(".text").html($(this).html());
+    $(this).parent().parent().parent().parent().removeClass("selectcontent_current");
+  }
+  );
+}
+);
+var current_select;
+var isover;
