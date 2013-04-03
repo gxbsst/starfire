e0ec66d307a25e0aabe358fad18d9ca88a8ed28b
gxbsst <gxbsst@gmail.com>
2013-04-03 10:35:24 +0800

    Init porject.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Starfire::Application.config.secret_token = 'ce55d7ed5c06662629800a07c76a3299d511a90646be1f255e91e0c2bbab1c7102140150d85d389cc6f04e2aff12d8d438a4544d06aafb7d0576b4f337c06b3e'
