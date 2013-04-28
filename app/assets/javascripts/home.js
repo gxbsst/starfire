f2da1844cec182ed8eefad2e72e876d04c6e98de
gxbsst <gxbsst@gmail.com>
2013-04-28 11:31:38 +0800

    fixed js about slide sync number for ie 8

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,53 +1,34 @@
-// KV SLIDE SHOW
-function showAuto() {
+$(document).ready(function(){
+  $("#banner_list a:not(:first-child)").hide();
+  var t = n = 0, count;
+  count = $("#banner_list a").length;
+  $("#banner li.nob").live('click',function() {
+    var i = $(this).text() - 1; //获取Li元素内的值，即1，2，3，4
+    var n = i;
+    if (i >= count) return;
+    var self = this;
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    $("#banner_list a").filter(":visible").fadeOut(500);
+    $('#banner_list').children().eq(i).fadeIn(1000);
+  });
+
+  function showAuto() {
     n = n >= (count - 1) ? 0 : ++n;
     $("#banner li.nob").eq(n).trigger('click');
-}
-var t = n = 0, count;
-count = $("#banner_list a").length;
+  }
 
-$("#banner_list a:not(:first-child)").hide();
-$("#banner_info").html($("#banner_list a:first-child").find("img").attr('alt'));
+  t = setInterval(showAuto, 8000);
 
-$("#banner_info").click(function() {
-    window.open($("#banner_list a:first-child").attr('href'), "_blank")
-});
-
-$("#banner li.nob").click(function() {
-    var i = $(this).text() - 1; //获取Li元素内的值，即1，2，3，4
-    n = i;
-    if (i >= count) return;
-    $("#banner_info").html($("#banner_list a").eq(i).find("img").attr('alt'));
-    $("#banner_info").unbind().click(function() {
-        window.open($("#banner_list a").eq(i).attr('href'), "_blank")
-    });
-    $("#banner_list a").filter(":visible").fadeOut(500).parent().children().eq(i).fadeIn(1000);
-    $(this).css({
-        "background": "#fff",
-        'color': '#000'
-    }).siblings().css({
-            "background": "#000",
-            'color': '#fff'
-        });
-});
-
-t = setInterval("showAuto()", 8000);
-
-$("#banner").hover(function() {
+  $("#banner").hover(function() {
     clearInterval(t)
-}, function() {
-    t = setInterval("showAuto()", 8000);
+  }, function() {
+    t = setInterval(showAuto, 8000);
+  });
+  // slide show end 
+
+  $("form .scb_btn > .searchbtn").click(function(){
+    $(this).closest('form').submit();
+  });
+
 });
-// KV SLIDE SHOW END
-
-
-
-
-
-$(document).ready(function(){
-
-    $("form .scb_btn > .searchbtn").click(function(){
-        $(this).closest('form').submit();
-    });
-});
-
