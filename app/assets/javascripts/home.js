5bb29eed790bbaad0f6fae2acf83af9d8d171218
gxbsst <gxbsst@gmail.com>
2013-08-01 10:48:34 +0800

    update home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,34 +1,14 @@
-$(document).ready(function(){
-  $("#banner_list a:not(:first-child)").hide();
-  var t = n = 0, count;
-  count = $("#banner_list a").length;
-  $("#banner li.nob").live('click',function() {
-    var i = $(this).text() - 1; //获取Li元素内的值，即1，2，3，4
-    var n = i;
-    if (i >= count) return;
-    var self = this;
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
-    $("#banner_list a").filter(":visible").fadeOut(500);
-    $('#banner_list').children().eq(i).fadeIn(1000);
-  });
+for el in $('.menus').find('img') {
+    $(el).mouseenter(function(e){
+        origin_name =  $(this).attr('src');
+        current_name = origin_name.match(/menu\d/) + '-o.png';
+        $(this).attr('src', 'images/' + current_name);
+    });
 
-  function showAuto() {
-    n = n >= (count - 1) ? 0 : ++n;
-    $("#banner li.nob").eq(n).trigger('click');
-  }
+    $(el).mouseleave(function(e){
+        origin_name =  $(this).attr('src');
+        current_name = origin_name.match(/menu\d/) + '.png';
+        $(this).attr('src', 'images/' + current_name);
+    });
 
-  t = setInterval(showAuto, 8000);
-
-  $("#banner").hover(function() {
-    clearInterval(t)
-  }, function() {
-    t = setInterval(showAuto, 8000);
-  });
-  // slide show end 
-
-  $("form .scb_btn > .searchbtn").click(function(){
-    $(this).closest('form').submit();
-  });
-
-});
+}
